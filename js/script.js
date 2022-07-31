04124b5832c4d44d3f4529b73759fc31cc96cf5c
Rajiv <22rajeev22@gmail.com>
2022-07-31 08:11:12 +0530

    first commit

---
+++ js/script.js
@@ -0,0 +1,39 @@
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slides[slideIndex-1].style.display = "block";
+}
+
+let slidenum = 0;
+showSlider();
+
+function showSlider() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slidenum++;
+  if (slidenum > slides.length) {slidenum = 1}
+  slides[slidenum-1].style.display = "block";
+  setTimeout(showSlider, 5000); // Change image every 2 seconds
+}
